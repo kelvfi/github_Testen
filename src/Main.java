331065d2cb0312f549bd610cade3f847a782d68a
kelvfi <126086807+kelvfi@users.noreply.github.com>
2024-02-15 11:11:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\n#### Zahlentester ####");
+        System.out.println("\n#### Zahlenster ####");
         System.out.println("Geben Sie die Nummer ein die Sie überprüfen wollen:");
         int eingabe = scan.nextInt();
         int sum = eingabe % 2; //Wenn 0 Rauskommt ist es eine Gerade Zahl
